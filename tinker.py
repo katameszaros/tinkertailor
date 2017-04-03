1439d5ec207d5d265f474a880d44d10b2055e799
Kata Meszaros <kata.meszaros92@gmail.com>
2017-04-03 23:48:04 +0200

    finalised based on conditions

--- tinker.py
+++ tinker.py
@@ -1,14 +1,18 @@
 
 def tinker(count, player_num):
     players = list(range(1, player_num+1))
-    prev = 0
-    index = 0
-    while len(players)>1:
-        print(players)
-        index = (prev+count-1) % len(players)
+    previous_index = 0
+    result = []
+    while len(players)>0:
+        index = (previous_index + count - 1) % len(players)
+        result.append(players[index])
         players.remove(players[index])
-        prev = index
-    print(players)
+        previous_index = index
+    return result
 
-tinker(3,5)
+
+def main():
+    print(tinker(count=3, player_num=5))
+
+main()
 
